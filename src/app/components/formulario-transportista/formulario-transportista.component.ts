1abeb603270080412ad6f1a07354ee91c1d9227f
Xavier Yanza <xavier.y.2013@gmail.com>
2020-04-24 02:32:20 -0500

    Metodo post transportista

--- src/app/components/formulario-transportista/formulario-transportista.component.ts
+++ src/app/components/formulario-transportista/formulario-transportista.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Transportista } from '../../model/transportista.model';
+import { TransportistaService } from '../../servicios/transportista.service';
+import { Direccion } from '../../model/direccion.model';
 
 @Component({
   selector: 'app-formulario-transportista',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-transportista.component.css']
 })
 export class FormularioTransportistaComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor(private _builder: FormBuilder, private transportistaService:TransportistaService) {
+    this.signupForm = this._builder.group(
+      {
+        direccion: ['', Validators.required]
+      }
+    )
+ 
+    console.log("correcto")  
+   }
 
   ngOnInit(): void {
+    this.getQuery();
   }
+
+  enviar(values) {
+    console.log(values);
+
+    return values;
+
+  }
+
+  getQuery() {
+    this.transportistaService.getDate(this.signupForm.value.direccion)
+    
+        this.transportistaService.getCategorias().subscribe((data:Transportista)=>{
+          console.log(data);
+          
+        });
+    
+        //idCategoria: this.signupForm.value.idCategoria,
+        console.log(this.signupForm.value.nombre);
+
+    
+      }
+
 
 }
--- src/app/servicios/transportista.service.ts
+++ src/app/servicios/transportista.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient,HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { Injectable } from '@angular/core'
 import { Transportista } from '../model/transportista.model';
@@ -13,6 +13,7 @@ import { environment } from '../../environments/environment';
 
 export class TransportistaService {
     baseURL = environment.apiURL + 'conductor/'
+    direccionin:string
     constructor( private http: HttpClient){
 
     }
@@ -38,7 +39,30 @@ export class TransportistaService {
         
 
     }
+    getDate(direccionget:string){
 
+        this.direccionin=direccionget
+    }
+    postQuery(query: string) {
+        const url = `http://localhost:9898/${query}`;
+        console.log(url)
+
+        let body = {
+           direccion:this.direccionin
+        }
+        let json = JSON.stringify(body);
+        let params = json;
+        let headers = new HttpHeaders().set('Content-Type',' application/json ');
+        console.log("body");
+        console.log(params);
+        return this.http.post(url, params, {headers: headers});
+
+    }
+    getCategorias(): Observable<any> {
+        const url = 'empleado';
+       
+        return this.postQuery(url);
+    }
 
 
 }
